0f9e749a34edd0a657e03017f768e10847166471
Shiplu Mokaddim <shiplu@mokadd.im>
2020-05-13 23:11:51 +0200

    Refactor tests

--- tests/test_entities.py
+++ tests/test_entities.py
@@ -9,6 +9,16 @@ from rover.entities import RoverState
 
 
 class TestRover(TestCase):
+    def assertRover(self, rover, grid=None, location=None, direction=None, state=None):
+        if grid is not None:
+            self.assertEqual(rover.grid, grid)
+        if location is not None:
+            self.assertEqual(rover.location, location)
+        if direction is not None:
+            self.assertEqual(rover.direction, direction)
+        if state is not None:
+            self.assertEqual(rover.state, state)
+
     def test_hashable(self):
         g = Grid(3, 3)
         r1 = Rover(g, Location(1, 1), Direction.N)
@@ -35,62 +45,45 @@ class TestRover(TestCase):
         g = Grid(3, 3)
         r1 = Rover(g, Location(2, 2), Direction.N)
         self.assertTrue(r1.move(Movement.M))
-        self.assertEqual(r1.grid, g)
-        self.assertEqual(r1.location, Location(2, 3))
-        self.assertEqual(r1.direction, Direction.N)
+        self.assertRover(r1, g, Location(2, 3), Direction.N)
 
     def test_move_east(self):
         g = Grid(3, 3)
         r1 = Rover(g, Location(2, 2), Direction.E)
         self.assertTrue(r1.move(Movement.M))
-        self.assertEqual(r1.grid, g)
-        self.assertEqual(r1.location, Location(3, 2))
-        self.assertEqual(r1.direction, Direction.E)
+        self.assertRover(r1, g, Location(3, 2), Direction.E)
 
     def test_move_south(self):
         g = Grid(3, 3)
         r1 = Rover(g, Location(2, 2), Direction.S)
         self.assertTrue(r1.move(Movement.M))
-        self.assertEqual(r1.grid, g)
-        self.assertEqual(r1.location, Location(2, 1))
-        self.assertEqual(r1.direction, Direction.S)
+        self.assertRover(r1, g, Location(2, 1), Direction.S)
 
     def test_move_west(self):
         g = Grid(3, 3)
         r1 = Rover(g, Location(2, 2), Direction.W)
         self.assertTrue(r1.move(Movement.M))
-        self.assertEqual(r1.grid, g)
-        self.assertEqual(r1.location, Location(1, 2))
-        self.assertEqual(r1.direction, Direction.W)
+        self.assertRover(r1, g, Location(1, 2), Direction.W)
 
     def test_move(self):
         g = Grid(3, 3)
         r1 = Rover(g, Location(1, 1), Direction.N)
         r1.move(Movement.M, Movement.R, Movement.M, Movement.M)
-        self.assertEqual(r1.grid, g)
-        self.assertEqual(r1.location, Location(3, 2))
-        self.assertEqual(r1.direction, Direction.E)
-        self.assertEqual(r1.state, RoverState.OPERATIONAL)
+        self.assertRover(r1, g, Location(3, 2), Direction.E, RoverState.OPERATIONAL)
 
     def test_move_collision(self):
         g = Grid(3, 3)
         r1 = Rover(g, Location(1, 1), Direction.N)
         r2 = Rover(g, Location(2, 2), Direction.S)
         self.assertFalse(r1.move(Movement.M, Movement.R, Movement.M, Movement.M))
-        self.assertEqual(r1.grid, g)
-        self.assertEqual(r1.location, Location(1, 2))
-        self.assertEqual(r1.direction, Direction.E)
-        self.assertEqual(r1.state, RoverState.STOPPED)
+        self.assertRover(r1, g, Location(1, 2), Direction.E, RoverState.STOPPED)
         self.assertEqual(r2.state, RoverState.OPERATIONAL)
 
     def test_move_border_collision(self):
         g = Grid(3, 3)
         r1 = Rover(g, Location(1, 1), Direction.N)
         self.assertFalse(r1.move(Movement.M, Movement.M, Movement.M))
-        self.assertEqual(r1.grid, g)
-        self.assertEqual(r1.location, Location(1, 3))
-        self.assertEqual(r1.direction, Direction.N)
-        self.assertEqual(r1.state, RoverState.STOPPED)
+        self.assertRover(r1, g, Location(1, 3), Direction.N, RoverState.STOPPED)
 
     def test_disabled_movement(self):
         g = Grid(3, 3)
