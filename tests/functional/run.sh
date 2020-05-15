3b804bf93a11025d98b4322d18234c9bdbcd6a79
Shiplu Mokaddim <shiplu@mokadd.im>
2020-05-15 15:36:57 +0200

    add test cases and test runner

---
+++ tests/functional/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+TEST_CASE_DIR=$(dirname $(realpath $0))
+PROJECT_ROOT="$(dirname $(dirname ${TEST_CASE_DIR}))"
+
+export PYTHONPATH=${PYTHONPATH:-$PROJECT_ROOT}
+
+for input in "${TEST_CASE_DIR}"/input*.txt
+do
+    output=$(echo "${input}" | sed -r 's/input(.*)\.txt/output\1.txt/')
+    echo -n "${input:$((${#TEST_CASE_DIR} + 1))} ... "
+    if cmp <(python ui/cli.py -f text "${input}") <"${output}"
+    then
+        echo PASSED
+    else
+        echo FAILED
+    fi
+done | column -t
